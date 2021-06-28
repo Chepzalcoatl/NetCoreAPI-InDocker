eafca0cc8444ca056b76d4fce47bc77d6bcec9a1
Eduardo Barron <st.buendia@gmail.com>
2021-06-28 12:26:55 -0500

    feature: api with swagger

---
+++ API/DTO/UsersDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTO
+{
+    public class UsersDto
+    {
+        public string UserName { get; set; }
+        public string Hobbies { get; set; }
+        public string Location { get; set; }
+    }
+}
